736e27659fd9e049a07bbbf1465c640085022005
Sai Deekshith Goud <saideekshithgoudsudhagona@gmail.com>
2026-07-12 14:45:32 +0530

    Debug loading

--- app.py
+++ app.py
@@ -68,10 +68,10 @@ unsafe_allow_html=True
 # LOAD MODEL
 # ---------------------------------------------------
 
-# model = joblib.load("models/model.pkl")
-# encoders = joblib.load("models/encoders.pkl")
-st.success("App Started Successfully")
-st.stop()
+model = joblib.load("models/model.pkl")
+encoders = joblib.load("models/encoders.pkl")
+# st.success("App Started Successfully")
+# st.stop()
 # ---------------------------------------------------
 # LOAD DATASET
 # ---------------------------------------------------
